50deaedb194a6868739b6b9ae4c1a33a18aafeea
Olena Piankova <el.piankova@gmail.com>
2017-11-27 15:54:21 +0200

    Added all verifications

--- tests/test_group_create.py
+++ tests/test_group_create.py
@@ -1,9 +1,10 @@
 def test_group_create(app, login_admin, group, db):
-    old_list = db.get_groups()
+    old_group_list = db.get_groups()
     app.open_group_page()
     app.create_group(group)
-    # TODO: verification of message
+    assert app.group_message_page.is_this_page()
+    assert "A new group has been entered into the address book." in app.group_message_page.message.text
     app.return_to_group_page()
-    new_list = db.get_groups()
-    assert len(new_list) == len(old_list) + 1
-    assert group == new_list[-1]
+    new_group_list = db.get_groups()
+    assert len(new_group_list) == len(old_group_list)+1, "There are not new group {}".format(group)
+    assert group == new_group_list[-1],  "Group in db is not like one was created"
--- tests/test_group_delete.py
+++ tests/test_group_delete.py
@@ -2,9 +2,15 @@ import random
 
 
 def test_delete_group(app, login_admin, check_groups_exist, db):
+    old_group_list = db.get_groups()
     index = random.randrange(app.count_groups())
     app.open_group_page()
-    app.delete_group_by_number(index)
-    # TODO: verification of message
+    group = app.select_group_by_number(index)
+    app.delete_group()
+    assert app.group_message_page.is_this_page()
+    assert "Group has been removed." in app.group_message_page.message.text
     app.return_to_group_page()
-    # TODO: Verification that group added
+    new_group_list = db.get_groups()
+    assert len(new_group_list) == len(old_group_list)-1
+    assert group not in new_group_list
+
